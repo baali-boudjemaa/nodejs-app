7687b4483c4bc2e0a72cc8013624eda08493c0f3
baali-boudjemaa <baaliboudjemaaens@gmail.com>
2021-08-01 06:31:29 -0700

    add more files

--- server.js
+++ server.js
@@ -1,3 +1,23 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const cors = require("cors");
+
+const app = express();
+
+var corsOptions = {
+  origin: "http://example.com"
+  ,
+  optionsSuccessStatus: 200
+};
+
+app.use(cors(corsOptions));
+
+// parse requests of content-type - application/json
+app.use(bodyParser.json());
+
+// parse requests of content-type - application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: true }));
+
 const db = require("./app/models");
 const controller = require("./app/controllers/tutorial.controller");
 
@@ -64,4 +84,24 @@ const run = async () => {
 db.sequelize.sync({ force: true }).then(() => {
   console.log("Drop and re-sync db.");
   run();
+});
+const db = require("./app/models");
+
+
+// // drop the table if it already exists
+// db.sequelize.sync({ force: true }).then(() => {
+//   console.log("Drop and re-sync db.");
+// });
+
+// simple route
+app.get("/", (req, res) => {
+  res.json({ message: "Welcome to bezkoder application." });
+});
+
+require("./app/routes/turorial.routes")(app);
+
+// set port, listen for requests
+const PORT = process.env.PORT || 8080;
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}.`);
 });
