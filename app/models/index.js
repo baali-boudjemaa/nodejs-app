40042796a90303d4d8c1e588fdcc12bfdbd62ac0
baali-boudjemaa <baaliboudjemaaens@gmail.com>
2021-08-01 07:53:47 -0700

    add more files

--- app/models/index.js
+++ app/models/index.js
@@ -19,13 +19,13 @@ const db = {};
 db.Sequelize = Sequelize;
 db.sequelize = sequelize;
 
-db.tutorials = require("./tutorial.model.js")(sequelize, Sequelize);
-db.comments = require("./comment.model.js")(sequelize, Sequelize);
+//db.tutorials = require("./tutorial.model.js")(sequelize, Sequelize);
+//db.comments = require("./comment.model.js")(sequelize, Sequelize);
 
-db.tutorials.hasMany(db.comments, { as: "comments" });
+/* db.tutorials.hasMany(db.comments, { as: "comments" });
 db.comments.belongsTo(db.tutorials, {
   foreignKey: "tutorialId",
   as: "tutorial",
-});
+}); */
 
 module.exports = db;
--- app/models/user.model.js
+++ app/models/user.model.js
@@ -1,9 +1,21 @@
 module.exports = (sequelize, DataTypes) => {
     const User = sequelize.define("user", {
-        name: {
+        id: {
+            type: DataTypes.INTEGER,
+            primaryKey: true,
+            autoIncrement: true // Automatically gets converted to SERIAL for postgres
+        },
+        username: {
             type: DataTypes.STRING
         },
-        text: {
+        email: {
+            type: DataTypes.STRING
+        }
+        ,
+        email: {
+            type: DataTypes.STRING
+        },
+        image: {
             type: DataTypes.STRING
         }
     });
